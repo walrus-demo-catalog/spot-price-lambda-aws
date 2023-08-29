c6c9a78c2539b11726d8e5b7d9595a674044a44d
pengjiang80 <peng.jiang@hotmail.com>
2023-08-29 16:17:14 +0800

    Add more instance types

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-# @options ["g4dn.xlarge","g4dn.2xlarge","g4dn.4xlarge"]
+# @options ["g4dn.xlarge","g4dn.2xlarge","g4dn.4xlarge","c5.2xlarge","c5.4xlarge"]
 variable "instance_type" {
   type = string
   description = "Instance type"
